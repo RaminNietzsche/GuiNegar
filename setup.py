60a3e13c8ab4e8342f2c869cb816cfbeaca1ae3e
RaminNietzsche <Ramin.najarbashi@gmail.com>
2012-08-31 17:19:06 +0430

    tiny bug ;)

--- setup.py
+++ setup.py
@@ -4,18 +4,31 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 setup(
-    name = "GNegar",
-    version = "0.1",
-    author = "Ramin Najjarbashi",
-    author_email = "Ramin.Najarbashi@gmail.com",
-    packages = find_packages() + ['lib'],
-    scripts=['bin/GNegar_Main.py'],
-    description = "Negar is a spell corrector and Persian text editor",
-    license = "GPL",
-    keywords = "spellcheck Persian editor",
-    url = "http://raminnietzsche.github.com/GuiNegar/",
-    long_description=read
-    ('README.txt'),    
+        name = "GNegar",
+        version = "0.1",
+        author = "Ramin Najjarbashi",
+        author_email = "Ramin.Najarbashi@gmail.com",
+        packages = find_packages() + ['lib'],
+        scripts=['bin/gnegar'],
+        description = "Negar is a spell corrector and Persian text editor",
+        license = "GPL",
+        url = "http://RaminNietzsche.github.com/GuiNegar",
+        long_description=read
+            ('README'),
+ 	keywords=['spellcheck','Persian','editor','python'],
+ 	data_files = [('share/doc/gnegar',['README', 'COPYING', 'CHANGES']),
+ 	('share/man/man1/', ['man/gnegar.1']) ],
+ 	classifiers=[
+ 	'Development Status :: 0 - Beta',
+ 	'Intended Audience :: Persian Users',
+ 	'Intended Audience :: Persian Programmers',
+	'License :: OSI Approved :: GNU General Public License v3',
+ 	'Operating System :: POSIX',
+ 	'Programming Language :: Python',
+ 	'Topic :: Editor',
+ 	'Topic :: Persian',
+ 	'Topic :: Spellcheck'
+ 	],    
 )
 
 
